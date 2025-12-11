617b8be3afe38806134b749954347d63ca92f0b4
Jairo Suarez <jairosp@amazon.com>
2025-12-11 15:49:49 +0100

    chore: configure AWS S3 + CloudFront deployment with GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://d3my5mfsjwe1or.cloudfront.net/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
